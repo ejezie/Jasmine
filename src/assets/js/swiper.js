8db097a8595fdc3cf0176515a7ff10339591e14b
EJEZIE CHINEDU EMMANUEL <54438361+ejezie@users.noreply.github.com>
2021-10-11 12:03:00 +0100

    setting up slider

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -2,35 +2,27 @@ const swiper = new Swiper(".swiper-one", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  speed:1000,
+    autoplay:
+    {
+      delay: 1000,
+    },
 
-  autoplay: 
-  {
-    delay: 30000,
-  },
-
+  // If we need pagination
   pagination: {
     el: ".page",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".next-one",
-    prevEl: ".prev-one",
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".scrollbar",
   },
 });
 
-const swiperTwo = new Swiper(".swiper-two", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  speed:1000,
-  autoplay: 
-  {
-    delay: 1000,
-  },
 
-  // If we need pagination
-  pagination: {
-    el: ".pagination",
-  },
-});
