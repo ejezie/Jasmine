8c0183e24b438e22d9c88d449e47046acf9576c8
EJEZIE CHINEDU EMMANUEL <54438361+ejezie@users.noreply.github.com>
2021-10-13 15:31:43 +0100

    Updating swiper config

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -7,6 +7,10 @@ const swiper = new Swiper(".swiper-one", {
     {
       delay: 4000,
     },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
 
   // If we need pagination
   pagination: {
@@ -24,3 +28,30 @@ const swiper = new Swiper(".swiper-one", {
     el: ".scrollbar",
   },
 });
+
+const swiperTwo = new Swiper('.swiper-two', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  speed:4000,
+  autoplay:
+  {
+    delay: 4000,
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    // when window width is >= 320px
+    400: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    960: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1250: {
+      slidesPerView: 3,
+    }
+  }
+});
+
